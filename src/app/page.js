d2a9246a6fd2806b172a0649bac0d8581195730a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-09-01 14:39:29 +0530

    vission and mission conent will be added in the index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import VisionMissionSection from "@/components/VisionMissionSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <VisionMissionSection/>
     </div>
   );
 }
